4d14dc09a486fb086a96c3affbf5008de33ff140
ZachParris <zacharyjparris@gmail.com>
2016-10-14 14:50:04 -0500

    added student lists

--- RepoQuiz/DAL/NameGenerator.cs
+++ RepoQuiz/DAL/NameGenerator.cs
@@ -10,5 +10,20 @@ namespace RepoQuiz.DAL
         // This class should be used to generate random names and Majors for Students.
         // This is NOT your Repository
         // All methods should be Unit Tested :)
+        private List<string> FirstName = new List<string>
+        {
+            "Zach", "Trent", "Matt", "Benje", "Marcelo", "Molly", "Dorna", "Callan", "Brad", "Norm"
+        };
+
+        private List<string> LastName = new List<string>
+        {
+            "Parris", "Smith", "Ater", "Casper", "West", "Morrison", "Bruton", "Macdonald", "Bailey", "Chan"
+        };
+
+        private List<string> Majors = new List<string>
+        {
+            "Computer Science", "Literature", "Physics", "Marketing", "Art", "Political Science", "Polish", "Philosophy"
+        }
+
     }
 }
