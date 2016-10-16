5592226b075724f93154da96d8e23c48ff12211b
ZachParris <zacharyjparris@gmail.com>
2016-10-15 22:52:00 -0500

    building repo with mock tests

--- RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
+++ RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
@@ -1,10 +1,100 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoQuiz.DAL;
+using System.Collections.Generic;
+using RepoQuiz.Models;
+using Moq;
+using System.Data.Entity;
+using System.Linq;
 
 namespace RepoQuiz.Tests.DAL
 {
     [TestClass]
     public class StudentRepositoryTests
     {
+        Mock<StudentContext> mock_context { get; set; }
+        Mock<DbSet<Student>> mock_student_table { get; set; }
+        List<Student> student_list { get; set; } // Fake
+        StudentRepository repo { get; set; }
+
+        public void ConnectMocksToDatastore()
+        {
+            var queryable_list = student_list.AsQueryable();
+
+            // Lie to LINQ make it think that our new Queryable List is a Database table.
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.Provider).Returns(queryable_list.Provider);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.Expression).Returns(queryable_list.Expression);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.ElementType).Returns(queryable_list.ElementType);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.GetEnumerator()).Returns(() => queryable_list.GetEnumerator());
+
+            // Have our Author property return our Queryable List AKA Fake database table.
+            mock_context.Setup(c => c.Students).Returns(mock_student_table.Object);
+
+            mock_student_table.Setup(t => t.Add(It.IsAny<Student>())).Callback((Student a) => student_list.Add(a));
+            mock_student_table.Setup(t => t.Remove(It.IsAny<Student>())).Callback((Student a) => student_list.Remove(a));
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Create Mock BlogContext
+            mock_context = new Mock<StudentContext>();
+            mock_student_table = new Mock<DbSet<Student>>();
+            student_list = new List<Student>(); // Fake
+            repo = new StudentRepository(mock_context.Object);
+
+            ConnectMocksToDatastore();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            repo = null; 
+        }
+
+        [TestMethod]
+        public void RepoEnsureCanCreateInstance()
+        {
+            StudentRepository repo = new StudentRepository();
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void RepoEnsureRepoHasContext()
+        {
+            StudentRepository repo = new StudentRepository();
+
+            StudentContext actual_context = repo.Context;
+
+            Assert.IsInstanceOfType(actual_context, typeof(StudentContext));
+        }
+
+        [TestMethod]
+        public void RepoEnsureWeHaveNoStudents()
+        {
+            // Arrange
+
+
+            // Act
+            List<Student> actual_student = repo.GetStudents();
+            int expected_student_count = 0;
+            int actual_student_count = actual_student.Count;
+
+            // Assert
+            Assert.AreEqual(expected_student_count, actual_student_count);
+        }
+
+        [TestMethod]
+        public void RepoEnsureAddStudentToDatabase()
+        {
+
+            Student my_student = new Student { FirstName = "Kurt", LastName = "Russel", Major = "English" }; 
+            repo.AddStudents(my_student);
+            int actual_student_count = repo.GetStudents().Count;
+            int expected_student_count = 1;
+            Assert.AreEqual(expected_student_count, actual_student_count);
+        }
+
+        
     }
 }
--- RepoQuiz/DAL/NameGenerator.cs
+++ RepoQuiz/DAL/NameGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using RepoQuiz.Models;
 using System.Linq;
 using System.Web;
 
@@ -10,20 +11,20 @@ namespace RepoQuiz.DAL
         // This class should be used to generate random names and Majors for Students.
         // This is NOT your Repository
         // All methods should be Unit Tested :)
-        private List<string> FirstName = new List<string>
+        public List<string> FirstName = new List<string>
         {
             "Zach", "Trent", "Matt", "Benje", "Marcelo", "Molly", "Dorna", "Callan", "Brad", "Norm"
         };
 
-        private List<string> LastName = new List<string>
+        public List<string> LastName = new List<string>
         {
             "Parris", "Smith", "Ater", "Casper", "West", "Morrison", "Bruton", "Macdonald", "Bailey", "Chan"
         };
 
-        private List<string> Majors = new List<string>
+        public List<string> Majors = new List<string>
         {
             "Computer Science", "Literature", "Physics", "Marketing", "Art", "Political Science", "Polish", "Philosophy"
-        }
+        };
 
     }
 }
